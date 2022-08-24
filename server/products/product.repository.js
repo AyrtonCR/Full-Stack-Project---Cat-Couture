5cd2788798041c4e33ddc916cdf6551680bea28d
Ayrton Campbell-Reinheimer <ayrton.campbell-reinheimer@developersinstitute.co.nz>
2022-08-24 15:57:59 +1200

    create a functioning repository for the prodcut endpoint, might be missing something with the deconsturcted pool variable

---
+++ server/products/product.repository.js
@@ -0,0 +1,41 @@
+const db = require("../db");
+
+module.exports = {
+  getAllProducts: async () => {
+    try {
+      const result = await db.query(`
+      SELECT p.id, p.name, p.description, p.price, pc.name AS "categoryName", pi.name AS "imageName", pi.description AS "imageDescription"
+      FROM product p
+      LEFT JOIN product_category pc ON p.product_category_id = pc.id
+      LEFT JOIN product_image pi ON p.product_image_id = pi.id
+      ORDER BY p.id
+      `);
+      return result.rows;
+    } catch (error) {
+      throw Error(error);
+    }
+  },
+
+  getProducts: async (limit, page) => {
+    try {
+      if (page <= 0 || !page) {
+        throw new Error("page number must be greater than 0");
+      }
+      const offset = limit * (page - 1);
+      const result = await db.query(
+        `
+      SELECT p.id, p.name, p.description, p.price, pc.name AS "categoryName", pi.name AS "imageName", pi.description AS "imageDescription"
+      FROM product p
+      LEFT JOIN product_category pc ON p.product_category_id = pc.id
+      LEFT JOIN product_image pi ON p.product_image_id = pi.id
+      ORDER BY p.id
+      LIMIT $1 OFFSET $2
+      `,
+        [limit, offset]
+      );
+      return result.rows;
+    } catch (error) {
+      throw Error(error);
+    }
+  },
+};
